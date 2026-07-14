32533458a866287163a42740b549a00db70cbdc0
CleaN1115 <nirerec@gmail.com>
2026-07-14 12:59:19 -0700

    Added database creation script

--- Inventory-Management-System/SQL/01-Create_Database.sql
+++ Inventory-Management-System/SQL/01-Create_Database.sql
@@ -1,1 +1,22 @@
+-- ===========================================================
+-- Company: Northwind Retail Group
+-- Project: Inventory Management System
+-- Ticket: #003
+-- Author: Claire N
+-- Description:
+-- Creates the InventoryManagement database.
+-- ===========================================================
 
+IF DB_ID('InventoryManagement') IS NULL
+BEGIN
+CREATE DATABASE InventoryManagement;
+PRINT 'InventoryManagement database created successfully.';
+END
+ELSE
+BEGIN
+PRINT 'InventoryManagement database already exists.';
+END
+GO
+
+USE InventoryManagement;
+GO
